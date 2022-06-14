cfe8d041e531ff52d81f7282668bdaf24303c32e
michael-petersen <petersen.michael.s@gmail.com>
2022-06-14 15:04:35 +0200

    Fix some optional argument flags

--- examples/plot_convergence_tests.py
+++ examples/plot_convergence_tests.py
@@ -1,6 +1,6 @@
 """
-plasma_plot.py
-    visualise some of the plasma calculations
+plot_convergence_tests.py
+    visualisation of some of the convergence tests for orbital elements
 
 """
 
@@ -51,12 +51,15 @@ A = np.genfromtxt("NINTconvergence.txt",delimiter=',')
 plt.figure(figsize=(4.5,3))
 
 # make predictions in grey, real values in black
+plt.text(0.,-2.7,'1/$N_\Omega^2$',color='grey',size=10)
 plt.plot(np.log10(A[1:,0]),np.log10(1/(A[1:,0]**2))-0.7,color='grey')
 plt.plot(np.log10(A[1:,0]),np.log10(np.abs(A[1:,1]-A[0,1])),color='black')
 
-plt.plot(np.log10(A[1:,0]),np.log10(1/(A[1:,0]**0))-8.,color='grey',linestyle='dashed')
+plt.text(0.,-11.0,'1/$N_\Omega^0$',color='grey',size=10)
+plt.plot(np.log10(A[1:,0]),np.log10(1/(A[1:,0]**0))-10.,color='grey',linestyle='dashed')
 plt.plot(np.log10(A[1:,0]),np.log10(np.abs(A[1:,2]-A[0,2])),color='black',linestyle='dashed')
 
+plt.text(0.,-5.9,'1/$N_\Omega^4$',color='grey',size=10)
 plt.plot(np.log10(A[1:,0]),np.log10(1/(A[1:,0]**4))-5.9,color='grey',linestyle='dotted')
 plt.plot(np.log10(A[1:,0]),np.log10(np.abs(A[1:,3]-A[0,3])),color='black',linestyle='dotted')
 
