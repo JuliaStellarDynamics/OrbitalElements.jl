172c492b1a91acc4406d2a02d00048e9ccefaf0c
Mathieu Roule <mathieu.roule@polytechnique.edu>
2022-08-08 14:43:22 +0200

    High order derivatives

--- src/Potentials/mestelzang.jl
+++ src/Potentials/mestelzang.jl
@@ -19,7 +19,7 @@ the truncated Mestel potential (flat rotation curve).
 """
 function mestel_psi(r::Float64,R0::Float64=1.,V0::Float64=1.,eps::Float64=0.01)
 
-    return 0.5 * (V0)^(2) * log( (r/R0)^(2)+(eps)^(2) )
+    return 0.5 * (V0)^(2) * log((r/R0)^(2) + (eps)^(2))
 end
 
 """
@@ -29,17 +29,39 @@ the truncated Mestel potential derivative.
 """
 function mestel_dpsi_dr(r::Float64,R0::Float64=1.,V0::Float64=1.,eps::Float64=0.01)
 
-    return (V0)^(2) / R0 * (r/R0) / ( (eps)^(2) + (r/R0)^(2) )
+    x = r/R0
+    return ((V0)^(2) / R0) * x / ((eps)^(2) + (x)^(2))
 end
 
 """
-    mestel_ddpsi_ddr(r[, R0, V0, epsilon])
+    mestel_d2psi_dr2(r[, R0, V0, epsilon])
 
 the truncated Mestel potential second derivative.
 """
 function mestel_ddpsi_ddr(r::Float64,R0::Float64=1.,V0::Float64=1.,eps::Float64=0.01)
 
-    return  (V0)^(2) / (R0)^(2) * ( (eps)^(2) - (r/R0)^2 ) / ( (eps)^(2) + (r/R0)^(2) )^(2)
+    x = r/R0
+    return  ((V0)^(2) / (R0)^(2)) * ((eps)^(2) - (x)^(2)) / ((eps)^(2) + (x)^(2))^(2)
+end
+"""
+    mestel_d3psi_dr3(r[, R0, V0, epsilon])
+
+the truncated Mestel potential third derivative.
+"""
+function mestel_d3psi_dr3(r::Float64,R0::Float64=1.,V0::Float64=1.,eps::Float64=0.01)
+
+    x = r/R0
+    return  ((V0)^(2) / (R0)^(3)) * 2 * x * ((x)^2 - 3*(eps)^(2)) / ((eps)^(2) + (x)^(2))^(3)
+end
+"""
+    mestel_d4psi_dr4(r[, R0, V0, epsilon])
+
+the truncated Mestel potential fourth derivative.
+"""
+function mestel_d4psi_dr4(r::Float64,R0::Float64=1.,V0::Float64=1.,eps::Float64=0.01)
+
+    x = r/R0
+    return  - 6 * ((V0)^(2) / (R0)^(4)) * ((x)^(4) - 6*(x)^(2)*(eps)^(2) + (eps)^(4)) / ((eps)^(2) + (x)^(2))^(4)
 end
 
 """
