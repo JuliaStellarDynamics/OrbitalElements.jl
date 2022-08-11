54e24c863d3b498454f7b1e3b86e67fbdb792123
michael-petersen <petersen.michael.s@gmail.com>
2022-08-11 15:01:38 -0400

    Add Plummer third, fourth derivatives

--- src/Potentials/plummer.jl
+++ src/Potentials/plummer.jl
@@ -44,6 +44,28 @@ function plummer_ddpsi_ddr(r::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG
 end
 
 """
+the plummer potential third derivative
+"""
+function plummer_dddpsi_dddr(r::Float64,bc::Float64=1.0,M::Float64=1.0,astronomicalG::Float64=1.0)::Float64
+    rbc = r^2 + bc^2
+    term1 = 15*(r^3)/(rbc^(7/2))
+    term2 = 9r/(rbc^(5/2))
+    return -astronomicalG*M*(-term1 + term2)
+end
+
+"""
+the plummer potential fourth derivative
+"""
+function plummer_ddddpsi_ddddr(r::Float64,bc::Float64=1.0,M::Float64=1.0,astronomicalG::Float64=1.0)::Float64
+    rbc = r^2 + bc^2
+    term1 = 105*(r^4)/(rbc^(9/2))
+    term2 = 90*(r^2)/(rbc^(7/2))
+    term3 = 9/(rbc^(5/2))
+    return -astronomicalG*M*(term1 - term2 + term3)
+end
+
+
+"""
 the central frequency for the Plummer potential
 """
 function plummer_Omega0(bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
