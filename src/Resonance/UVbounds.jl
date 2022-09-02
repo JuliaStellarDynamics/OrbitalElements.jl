d7f39f1ff9629e128843433282d1a58098ba5fe6
michael-petersen <petersen.michael.s@gmail.com>
2022-09-02 16:41:05 +0200

    Add Omega2 expansion value

--- src/Resonance/UVbounds.jl
+++ src/Resonance/UVbounds.jl
@@ -56,7 +56,7 @@ function FindWminWmax(n1::Int64,n2::Int64,
                       rmax::Float64=1.0e5)
 
     # define the function to extremise
-    ωncirc(x) = n1*Ω1circular(dψ,d2ψ,x)/Ω0 + n2*Ω2circular(dψ,x)/Ω0
+    ωncirc(x) = n1*Ω1circular(dψ,d2ψ,x)/Ω0 + n2*Ω2circular(dψ,d2ψ,x)/Ω0
 
     αmin, αmax = αminmax(dψ,d2ψ,rmin,rmax,Ω0=Ω0)
     xext = ExtremiseFunction(ωncirc,rmin,rmax)
@@ -64,24 +64,24 @@ function FindWminWmax(n1::Int64,n2::Int64,
     # The extreme values of n.Ω is either :
     #   - on the radial line, at α = αmin or αmax
     #   - along the circular velocity (extreme α included)
-    wmin,wmax = extrema([ωncirc(xext), ωncirc(rmin), ωncirc(rmax), (n1+0.5*n2)*αmin, (n1+0.5*n2)*αmax]) 
+    wmin,wmax = extrema([ωncirc(xext), ωncirc(rmin), ωncirc(rmax), (n1+0.5*n2)*αmin, (n1+0.5*n2)*αmax])
     return wmin,wmax
 end
 
 """αminmax(dψ/dr,d²ψ/dr²,rmin,rmax[, Ω0])
 maximal and minimal considered radial frequencies (rescaled)
 
-@ASSUMPTION: 
+@ASSUMPTION:
     - Ω1circular is a decreasing function of radius
 """
 function αminmax(dψ::Function,
                  d2ψ::Function,
                  rmin::Float64,
-                 rmax::Float64; 
+                 rmax::Float64;
                  Ω0::Float64=1.)
 
     @assert rmin < rmax "rmin >= rmax in αminmax function"
-    # Assumption : 
+    # Assumption :
     # Ω1circular is a decreasing function of radius
     return Ω1circular(dψ,d2ψ,rmax)/Ω0, Ω1circular(dψ,d2ψ,rmin)/Ω0
 end
@@ -118,12 +118,12 @@ function FindVminVmax(u::Float64,
 
     if (n2==0)
         #####
-        # (B9) Fouvry & Prunet : 1st inequality 
+        # (B9) Fouvry & Prunet : 1st inequality
         #####
         vmin = 0.5
-        
+
         #####
-        # (B9) Fouvry & Prunet : 2nd inequality 
+        # (B9) Fouvry & Prunet : 2nd inequality
         #####
         # put in guards for the edges
         αvmax = min(αmax,max(hval/n1,αmin))
@@ -135,7 +135,7 @@ function FindVminVmax(u::Float64,
         #####
         vmin = αmin
         vmax = αmax
-        
+
         #####
         # (B10) Fouvry & Prunet : 3rd inequality
         #####
@@ -158,7 +158,7 @@ function FindVminVmax(u::Float64,
         if n1*n2 > 0 # Do not search for vbound
             branch = 1
         else
-            # First look for vbound in the asked boundary 
+            # First look for vbound in the asked boundary
             vbound = FindVbound(n1,n2,dψ,d2ψ,Ω0=Ω0,rmin=rmin,rmax=rmax)
 
             # Extreme boundary to look for vbound
@@ -176,8 +176,8 @@ function FindVminVmax(u::Float64,
                     branch = 2
                 else
                     branch = 1
-                end   
-            else 
+                end
+            else
                 branch = 2
             end
         end
@@ -189,21 +189,21 @@ function FindVminVmax(u::Float64,
             if rootequation(αmin)*rootequation(αmax) < 0.
                 vlim = bisection(rootequation,αmin,αmax)
                 if hval*n2 > 0.
-                    vmin = max(vmin,vlim) 
+                    vmin = max(vmin,vlim)
                 elseif hval*n2 < 0.
-                    vmax = min(vmax,vlim) 
+                    vmax = min(vmax,vlim)
                 end
             end
         elseif branch == 2 # ωncirc(x) is not monotonic
             # Search crossing in [αmin,vbound]
             if rootequation(αmin)*rootequation(vbound) < 0.
-                vmin2 = bisection(rootequation,αmin,vbound) 
-                vmin = max(vmin,vmin2) 
+                vmin2 = bisection(rootequation,αmin,vbound)
+                vmin = max(vmin,vmin2)
             end
             # Search crossing in [vbound,αmax]
             if rootequation(vbound)*rootequation(αmax) < 0.
                 vmax2 = bisection(rootequation,vbound,αmax)
-                vmax = min(vmax,vmax2) 
+                vmax = min(vmax,vmax2)
             end
         end
     end
@@ -230,7 +230,7 @@ function FindVbound(n1::Int64,n2::Int64,
                     rmin::Float64=1.0e-8,
                     rmax::Float64=1.0e5)
 
-    # define the function to extremise 
+    # define the function to extremise
     ωncirc(x) = n1*Ω1circular(dψ,d2ψ,x) + n2*Ω2circular(dψ,x)
 
     xext = ExtremiseFunction(ωncirc,rmin,rmax)
