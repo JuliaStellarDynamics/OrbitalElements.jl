0220bcf737e252d551aa45f14a55f65762e4b1cf
Mathieu Roule <mathieu.roule@polytechnique.edu>
2022-09-22 10:13:56 +0200

    If onelining and variable names (UV mapping checked)

--- src/Utils/Integrators.jl
+++ src/Utils/Integrators.jl
@@ -2,34 +2,34 @@
 
 """
 function to perform a Simpson's 1/3 composite integration between -1<u<1
-@ATTENTION, K_MF must be even
+@ATTENTION, the number of steps K must be even
 fun is a function that takes u as an argument and returns the value of some function at that point
 this function can also take a function that passes an array and push them all forward at once.
 """
-function UnitarySimpsonIntegration(fun::Function;K_O::Int64=128)
+function UnitarySimpsonIntegration(fun::Function;
+                                   K::Int64=128)
 
     # specify the step size
-    Delta_u = 2.0 / K_O
+    δu = 2.0 / K
 
     # set up the launching point
     u   = -1.0
-    res = Delta_u .* fun(u) ./ 6.0
+    res = δu .* fun(u) ./ 6.0
 
     # add contributions from the interior points
-    for k = 1:(K_O-1)
-        u   += 0.5 * Delta_u
-        res = res .+ 2.0 .* Delta_u .* fun(u) ./ 3.0
-        u   += 0.5 * Delta_u
-        res = res .+ 1.0 .* Delta_u .* fun(u) ./ 3.0
+    for k = 1:(K-1)
+        u   += 0.5 * δu
+        res = res .+ 2.0 .* δu .* fun(u) ./ 3.0
+        u   += 0.5 * δu
+        res = res .+ 1.0 .* δu .* fun(u) ./ 3.0
     end
 
     # add the final points
-    u   += 0.5 * Delta_u
-    res = res .+ 2.0 .* Delta_u .* fun(u) ./ 3.0
-    u   += 0.5 * Delta_u
-    res = res .+ 1.0 .* Delta_u .* fun(u) ./ 6.0
+    u   += 0.5 * δu
+    res = res .+ 2.0 .* δu .* fun(u) ./ 3.0
+    u   += 0.5 * δu
+    res = res .+ 1.0 .* δu .* fun(u) ./ 6.0
 
     # return the value(s)
     return res
-
 end
