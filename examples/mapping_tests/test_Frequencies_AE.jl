3b42b3b379375963a7bcf78e633d404e0a73b585
Mathieu Roule <mathieu.roule@polytechnique.edu>
2022-12-09 17:30:42 +0100

    Update test files

--- examples/mapping_tests/test_Frequencies_AE.jl
+++ examples/mapping_tests/test_Frequencies_AE.jl
@@ -1,7 +1,7 @@
 import OrbitalElements
 using HDF5
 
-outdir="./"
+outdir="./data/"
 
 ####
 # Mestel
@@ -18,24 +18,26 @@ const Ω₀ = OrbitalElements.Ω₀Mestel(R0,V0)
 ####
 # Isochrone
 ####
-# modelname = "Isochrone"
-# bc, M, G = 1.,1.,1.
-# ψ(r::Float64)::Float64   = OrbitalElements.ψIsochrone(r,bc,M,G)
-# dψ(r::Float64)::Float64  = OrbitalElements.dψIsochrone(r,bc,M,G)
-# d2ψ(r::Float64)::Float64 = OrbitalElements.d2ψIsochrone(r,bc,M,G)
-# d3ψ(r::Float64)::Float64 = OrbitalElements.d3ψIsochrone(r,bc,M,G)
-# Ω0 = OrbitalElements.Omega0Isochrone(bc,M,G)
+# const modelname = "Isochrone"
+# const bc, M, G = 1.,1.,1.
+# const ψ(r::Float64)::Float64   = OrbitalElements.ψIsochrone(r,bc,M,G)
+# const dψ(r::Float64)::Float64  = OrbitalElements.dψIsochrone(r,bc,M,G)
+# const d2ψ(r::Float64)::Float64 = OrbitalElements.d2ψIsochrone(r,bc,M,G)
+# const d3ψ(r::Float64)::Float64 = OrbitalElements.d3ψIsochrone(r,bc,M,G)
+# const d4ψ(r::Float64)::Float64 = OrbitalElements.d4ψIsochrone(r,bc,M,G)
+# const Ω₀ = OrbitalElements.Ω₀Isochrone(bc,M,G)
 
 ####
 # Plummer
 ####
-# modelname = "Plummer"
-# bc, M, G = 1.,1.,1.
-# ψ(r::Float64)::Float64   = OrbitalElements.plummer_psi(r,bc,M,G)
-# dψ(r::Float64)::Float64  = OrbitalElements.plummer_dpsi_dr(r,bc,M,G)
-# d2ψ(r::Float64)::Float64 = OrbitalElements.plummer_ddpsi_ddr(r,bc,M,G)
-# d3ψ(r::Float64)::Float64 = OrbitalElements.plummer_dddpsi_dddr(r,bc,M,G)
-# Ω0 = OrbitalElements.plummer_Omega0(bc,M,G)
+# const modelname = "Plummer"
+# const bc, M, G = 1.,1.,1.
+# const ψ(r::Float64)::Float64   = OrbitalElements.ψPlummer(r,bc,M,G)
+# const dψ(r::Float64)::Float64  = OrbitalElements.dψPlummer(r,bc,M,G)
+# const d2ψ(r::Float64)::Float64 = OrbitalElements.d2ψPlummer(r,bc,M,G)
+# const d3ψ(r::Float64)::Float64 = OrbitalElements.d3ψPlummer(r,bc,M,G)
+# const d4ψ(r::Float64)::Float64 = OrbitalElements.d4ψPlummer(r,bc,M,G)
+# const Ω₀ = OrbitalElements.Ω₀Plummer(bc,M,G)
 
 #####
 # Parameters
@@ -66,8 +68,12 @@ function run_test!(filename::String,
                     na::Int64,ne::Int64,
                     ψ::Function,dψ::Function,d2ψ::Function,d3ψ::Function,d4ψ::Function,
                     params::OrbitalElements.OrbitsParameters)
-
     
+    #####
+    # Check filename
+    ######
+    file = h5open(filename,"w")
+    close(file)
 
     #####
     # (a,e) points to map to (α,β) 
@@ -156,26 +162,3 @@ run_test!(filename,
             na,ne,
             ψ,dψ,d2ψ,d3ψ,d4ψ,
             params)
-
-
-# a, e = 10., 1.
-# println("a = ",a," ; e = ",e)
-
-# Ω1, Ω2 = OrbitalElements.ComputeFrequenciesAE(ψ,dψ,d2ψ,d3ψ,d4ψ,a,e,params)
-# println("Ω1 = ",Ω1," ; Ω2 = ",Ω2)
-
-# α, β = Ω1/Ω₀, Ω2/Ω1
-# println("α = ",α," ; β = ",β)
-
-# Ω1n, Ω2n = params.Ω₀*α, params.Ω₀*α*β
-# println("Ω1n = ",Ω1n," ; Ω2n = ",Ω2n)
-
-# an, en, iter, tol = OrbitalElements.AEFromΩ1Ω2Brute(Ω1n,Ω2n,ψ,dψ,d2ψ,d3ψ,d4ψ,params)
-# println("an = ",an," ; en = ",en)
-# println("Iterations : ",iter," ; Tolerance : ",tol)
-
-# Ω1l, Ω2l = OrbitalElements.ComputeFrequenciesAE(ψ,dψ,d2ψ,d3ψ,d4ψ,an,en,params)
-# println("Ω1l = ",Ω1l," ; Ω2l = ",Ω2l)
-
-# αl, βl = Ω1l/Ω₀, Ω2l/Ω1l
-# println("αl = ",αl," ; βl = ",βl)
