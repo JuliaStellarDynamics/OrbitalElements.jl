562de7259a393827c551b6fe8ebe11d63a2c9ae8
michael-petersen <petersen.michael.s@gmail.com>
2024-03-07 21:22:13 +0000

    patch scale energy in isochrone distribution functions

--- src/distributions/isochrone.jl
+++ src/distributions/isochrone.jl
@@ -1,7 +1,7 @@
 """
 """
 function isochroneQROI(E::Float64,L::Float64,Ra::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
-    scaleEnergy = isochroneE0(bc,M,astronomicalG)
+    scaleEnergy = -astronomicalG*M/bc
     Q = (E + L^(2)/(2.0*Ra^(2)))/scaleEnergy # Computing the Q parameter
     return Q # Output
 end
@@ -12,7 +12,6 @@ Saha distribution function
 ra is the anisotropy radius
 """
 function isochroneSahaDF(E::Float64,L::Float64,ra::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
-    #scaleEnergy = isochroneE0(bc,M,astronomicalG)
 
     Q = isochroneQROI(E,L,ra,bc,M,astronomicalG)
     gamma = (bc/ra)^2
@@ -78,7 +77,7 @@ end
 jacobian for converting dF/dQ dQ/dE -> dF/dE
 """
 function isochronedQdEROI(E::Float64,L::Float64,Ra::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
-    scaleEnergy = isochroneE0(bc,M,astronomicalG)
+    scaleEnergy = -astronomicalG*M/bc
     return 1.0/scaleEnergy # Output
 end
 
@@ -89,7 +88,7 @@ jacobian for converting dF/dQ dQ/dL -> dF/dL
 @IMPROVE, does this need an extra factor of bc?
 """
 function isochronedQdLROI(E::Float64,L::Float64,Ra::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
-    scaleEnergy = isochroneE0(bc,M,astronomicalG)
+    scaleEnergy = -astronomicalG*M/bc
     return (L)/(scaleEnergy*Ra^(2)) # Output
 end
 
@@ -100,7 +99,7 @@ the isotropic DF
 """
 function isochroneisotropicDF(E::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
     # get the relevant scale energy
-    scaleEnergy = isochroneE0(bc,M,astronomicalG)
+    scaleEnergy = -astronomicalG*M/bc
 
     # rescale dimensionless energy to goes between 0 and 1/2
     mE = E/scaleEnergy
@@ -118,7 +117,7 @@ the isotropic DF derivative w.r.t. E
 function isochroneisotropicdDFdE(E::Float64,bc::Float64=1.,M::Float64=1.,astronomicalG::Float64=1.)
 
     # get the relevant scale energy
-    scaleEnergy = isochroneE0(bc,M,astronomicalG)
+    scaleEnergy = -astronomicalG*M/bc
 
     # rescale dimensionless energy to goes between 0 and 1/2
     mE = E/scaleEnergy
