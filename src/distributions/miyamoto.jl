3054e6e3ca93005cea51a7164b3104e292bf8cb4
Mathieu Roule <72818492+MathieuRoule@users.noreply.github.com>
2024-03-26 16:17:29 +0100

    Tested Miyamoto disc distribution

--- src/distributions/miyamoto.jl
+++ src/distributions/miyamoto.jl
@@ -5,42 +5,73 @@ using HypergeometricFunctions
 #   The Miyamoto DFs for Kuzmin-Toomre disc 
 #
 #####
-
 """
-    Miyamoto_DF(E, L)
+    miyamoto_distribution(E, L[, mM])
+
 Miyamoto distribution function for Kuzmin-Toomre disc.
 """
-function Miyamoto_DF(E::Float64,L::Float64;mM::Int64=1)
-
-    return (2*mM+3) / (2*(pi^2)) * (E^(2*mM+2)) * _₂F₁(-mM, -2-2*mM, 1/2, L^2/(2*E))
-end
-"""
-    Miyamoto_dDFdE(E, L)
-Miyamoto DF derivative w.r.t. E.
-"""
-function Miyamoto_dDFdE(E::Float64,L::Float64;mM::Int64=1)
-
-    return ((mM+1)*(2*mM+3) / (pi^2)) * (E^(2*mM)) * 
-                ( E * _₂F₁(-mM,-2-2*mM,1/2,L^2/(2*E))
-                - (L^2) * _₂F₁(-1-2*mM,1-mM,3/2,L^2/(2*E)) )
-end
-"""
-    Miyamoto_dDFdL(E, L)
-Miyamoto DF derivative w.r.t. L.
-"""
-function Miyamoto_dDFdL(E::Float64,L::Float64;mM::Int64=1)
-
-    return (2*mM*(mM+1)*(2*mM+3) / (pi^2)) * L * (E^(2*mM+1)) * _₂F₁(-1-2*mM,1-mM,3/2,L^2/(2*E))
+function miyamoto_distribution(E::Float64, L::Float64; mM::Int64=1)
+    return (
+        (2mM + 3)
+        * (-E)^(2mM + 2)
+        * _₂F₁(-mM, -2-2mM, 1/2, -L^2/(2E))
+        / (2 * pi^2)
+    )
 end
 
 """
-    Miyamoto_ndDFdJ(n1, n2, E, L, ndotOmega)
-Miyamoto DF derivative w.r.t. the actions J.
-"""
-function Miyamoto_ndDFdJ(n1::Int64,n2::Int64,E::Float64,L::Float64,ndotOmega::Float64;mM::Int64=1)
+    miyamoto_dFdE(E, L[, mM])
 
-    dDFdE = Miyamoto_dDFdE(E,L;mM=mM)
-    dDFdL = Miyamoto_dDFdL(E,L;mM=mM)
+Miyamoto distribution derivative w.r.t. energy.
+"""
+function miyamoto_dFdE(E::Float64, L::Float64; mM::Int64=1)
+    return (
+        E^(2mM)
+        * (1 + mM)
+        * (3 + 2mM)
+        * ( 
+            L^2 * mM * _₂F₁(-1-2mM, 1-mM, 3/2, -L^2/(2E))
+            + E * _₂F₁(-mM, -2-2mM, 1/2, -L^2/(2E))
+        )
+        / pi^2
+    )
+end
+
+"""
+    miyamoto_dFdL(E, L[, mM])
+
+Miyamoto distribution derivative w.r.t. angular momentum.
+"""
+function miyamoto_dFdL(E::Float64, L::Float64; mM::Int64=1)
+
+    return -(
+        2
+        * E^(1 + 2mM)
+        * L
+        * mM
+        * (1 + mM)
+        * (3 + 2mM)
+        * _₂F₁(-1-2mM, 1-mM, 3/2, -L^2/(2E))
+        / pi^2
+    )
+end
+
+"""
+    miyamoto_ndFdJ(n1, n2, E, L, ndotOmega[, mM])
+
+Miyamoto distribution derivatives w.r.t. the actions.
+"""
+function miyamoto_ndFdJ(
+    n1::Int64,
+    n2::Int64,
+    E::Float64,
+    L::Float64,
+    ndotOmega::Float64;
+    mM::Int64=1
+)
+
+    dFdE = miyamoto_dFdE(E, L; mM=mM)
+    dFdL = miyamoto_dFdL(E, L; mM=mM)
     
-    return ndotOmega*dDFdE + n2*dDFdL
+    return ndotOmega*dFdE + n2*dFdL
 end
