ab2e78ce4bc343f7cc170f411903eb4ecb5361a2
michael-petersen <petersen.michael.s@gmail.com>
2025-01-17 20:45:25 +0000

    update test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Test
 
 include("test_utils.jl")
 include("test_potentials.jl")
-#include("test_mappings.jl")
+include("test_mappings.jl")
 #include("test_monkey.jl")
 include("test_allocs.jl")
